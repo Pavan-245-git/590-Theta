3bfec806f443c9c91cce08a66464eb7185f2bc43
Pavan-245-git <sanapalapavansatwick@gmail.com>
2025-01-06 21:14:27 +0530

    Add files via upload

---
+++ Assignment 1 string methods.py
@@ -0,0 +1,49 @@
+# strings methods like upper, lower soon
+# strings are immutable 
+
+
+
+a = "pineapple"
+b = "PINEAPPLE"
+print(a.upper()) # converts into upper string
+print(b.lower()) # converts into lower string
+
+
+# strip
+c = "  hello brother  "
+print(c.strip())
+
+
+# rstrip : which is used to remove the mentioned string
+d = "pavan !!!"
+print(d.rstrip("!")
+
+
+#replace
+print(a.replace("pineapple","pavan"))
+
+#split
+e = "hello, bro how are you"
+print(e.split())
+
+#capitalize: capitalize the each starting word
+print(e.capitalize())
+
+
+# center
+
+print(a.center(50))
+
+#count : counting the specific word how many times repetiting
+print(a.count("a"))
+
+#find
+print(a.find("a")
+
+#index:
+f = "iam fine"
+print(f.index("fine"))
+
+#title
+print(f.title())
+
