4f514afc7f33961abf7bf8c0b14183eaf6182386
Pavan Satwick <sanapalapavansatwick@gmail.com>
2025-01-27 14:35:27 +0000

    string

---
+++ dataframe2.py
@@ -0,0 +1,6 @@
+import numpy as pd
+arr = arr = np.array([[[1, 1, 2, 3], [4, 4, 5, 6]], [[7, 8, 9, 9], [10, 11, 12, 13]]])
+print(arr)
+a= int(input("Enter number of rows: "))
+b= int(input("Enter the elements: "))
+print(arr[a,b])
